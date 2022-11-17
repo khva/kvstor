6d913210466ed5e56c47bf79f8d8c0c4481c10e8
khva <khvaonline@gmail.com>
2022-11-17 19:04:33 +0300

    first implementation

---
+++ include/kvstor.h
@@ -0,0 +1,334 @@
+﻿// kvstor.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <cassert>
+#include <chrono>
+#include <functional>
+#include <list>
+#include <mutex>
+#include <optional>
+#include <unordered_map>
+
+
+namespace kvstor
+{
+
+    template <class key_type, class value_type>
+    struct traits_t
+    {
+        using hash_t = std::hash<key_type>;
+        using kequal_t = std::equal_to<key_type>;
+
+        template <class item_type>
+        using alloc_t = std::allocator<item_type>;
+    };
+
+    template
+    <
+        class key_type,
+        class value_type,
+        class traits_type = traits_t<key_type, value_type>
+    >
+    class storage_t final
+    {
+    public:
+        using key_t = key_type;
+        using value_t = value_type;
+        using hash_t = traits_type::hash_t;
+        using kequal_t = traits_type::kequal_t;
+        using clock_t = std::chrono::steady_clock;
+        using time_point_t = clock_t::time_point;
+        using duration_t = clock_t::duration;
+
+        storage_t(size_t max_size, duration_t life_time) noexcept;
+        storage_t(const storage_t &) = delete;
+        storage_t(storage_t &&) = delete;
+        ~storage_t() noexcept;
+
+        storage_t operator=(const storage_t &) = delete;
+        storage_t operator=(storage_t &&) = delete;
+
+        void push(const key_t & key, value_t && value);
+        void push(const key_t & key, const value_t & value);
+
+        std::optional<value_t> find(const key_t & key) const;
+        std::optional<value_t> first() const;
+        std::optional<value_t> last() const;
+
+        void map(std::function<void (const key_t & key, value_t & value, duration_t life_time)> func);
+        void map(std::function<void (const key_t & key, const value_t & value, duration_t life_time)> func) const;
+
+        size_t size() const;
+        bool empty() const;
+
+        size_t max_size() const noexcept;
+        duration_t life_time() const noexcept;
+
+        void erase(const key_t& key);
+        void clear() noexcept;
+
+    private:
+        struct item_t
+        {
+            value_t         value;
+            key_t           key;
+            time_point_t    end_time;
+        };
+
+        using list_alloc_t = typename traits_t<key_t, value_t>::alloc_t<item_t>;
+        using list_t = std::list<item_t, list_alloc_t>;
+        using index_item_t = typename list_t::iterator;
+        using index_pair_t = std::pair<const key_t, index_item_t>;
+        using index_alloc_t = typename traits_t<key_t, value_t>::alloc_t<index_pair_t>;
+        using index_t = std::unordered_map<key_t, index_item_t, hash_t, kequal_t, index_alloc_t>;
+
+        void clear_outdated();
+
+        list_t  m_data;
+        index_t m_index;
+
+        mutable std::mutex  m_lock;
+
+        const size_t      m_max_size;
+        const duration_t  m_life_time;
+    };
+
+
+    template <class key_type, class value_type, class traits_type>
+    inline storage_t<key_type, value_type, traits_type>::storage_t(size_t max_size, duration_t life_time) noexcept
+    :   m_data()
+    ,   m_index()
+    ,   m_lock()
+    ,   m_max_size(max_size)
+    ,   m_life_time(life_time)
+    {
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    inline storage_t<key_type, value_type, traits_type>::~storage_t() noexcept
+    {
+        clear();
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    void storage_t<key_type, value_type, traits_type>::push(const key_t & key, value_t && value)
+    {
+        const std::lock_guard guard{ m_lock };
+
+        clear_outdated();
+        m_data.emplace_front(std::forward<value_t>(value), key, clock_t::now() + m_life_time);
+        auto found = m_index.find(key);
+
+        if (found == m_index.end())
+        {
+            m_index.emplace(key, m_data.begin());
+        }
+        else
+        {
+            assert(found->second->key == key);
+            m_data.erase(found->second);
+            found->second = m_data.begin();
+        }
+
+        if (m_data.size() > m_max_size)
+        {
+            m_index.erase(m_data.back().key);
+            m_data.pop_back();
+        }
+
+        assert(m_data.size() == m_index.size());
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    inline void storage_t<key_type, value_type, traits_type>::push(const key_t & key, const value_t & value)
+    {
+        push(key, value_t(value));
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    inline std::optional<value_type> storage_t<key_type, value_type, traits_type>::find(const key_t & key) const
+    {
+        const std::lock_guard guard{ m_lock };
+        auto found = m_index.find(key);
+
+        if (found == m_index.end())
+            return std::optional<value_t>{};
+
+        assert(found->second->key == key);
+        const time_point_t now = clock_t::now();
+
+        if (found->second->end_time <= now)
+            return std::optional<value_t>{};
+
+        return std::optional<value_t>{found->second->value};
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    inline std::optional<value_type> storage_t<key_type, value_type, traits_type>::first() const
+    {
+        const std::lock_guard guard{ m_lock };
+
+        if (m_data.empty())
+        {
+            assert(m_index.empty());
+            return std::optional<value_t>{};
+        }
+
+        return std::optional<value_t>{ m_data.front().value };
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    inline std::optional<value_type> storage_t<key_type, value_type, traits_type>::last() const
+    {
+        const std::lock_guard guard{ m_lock };
+        const_cast<storage_t<key_type, value_type, traits_type>*>(this)->clear_outdated();
+
+        if (m_data.empty())
+        {
+            assert(m_index.empty());
+            return std::optional<value_t>{};
+        }
+
+        const time_point_t now = clock_t::now();
+        const item_t& first = m_data.back();
+
+        return first.end_time < now ? std::optional<value_t>{} : std::optional<value_t>{ first.value };
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    void storage_t<key_type, value_type, traits_type>::map(std::function<void (const key_t & key, value_t & value, duration_t life_time)> func)
+    {
+        const std::lock_guard guard{ m_lock };
+        const time_point_t now = clock_t::now();
+
+        for (item_t & item : m_data)
+        {
+            if (item.end_time < now)
+                break;
+
+            const duration_t life_time = item.end_time - now;
+            func(item.key, item.value, life_time);
+        }
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    void storage_t<key_type, value_type, traits_type>::map(std::function<void (const key_t & key, const value_t & value, duration_t life_time)> func) const
+    {
+        const std::lock_guard guard{ m_lock };
+        const time_point_t now = clock_t::now();
+
+        for (const item_t & item : m_data)
+        {
+            if (item.end_time < now)
+                break;
+
+            const duration_t life_time = item.end_time - now;
+            func(item.key, item.value, life_time);
+        }
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    size_t storage_t<key_type, value_type, traits_type>::size() const
+    {
+        const std::lock_guard guard{ m_lock };
+        const_cast<storage_t<key_type, value_type, traits_type> *>(this)->clear_outdated();
+
+        const size_t size = m_data.size();
+        assert(size == m_index.size());
+
+        return size;
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    bool storage_t<key_type, value_type, traits_type>::empty() const
+    {
+        const std::lock_guard guard{ m_lock };
+        const_cast<storage_t<key_type, value_type, traits_type>*>(this)->clear_outdated();
+
+        const bool is_empty = m_data.empty();
+        assert(m_index.empty() == is_empty);
+
+        return is_empty;
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    inline size_t storage_t<key_type, value_type, traits_type>::max_size() const noexcept
+    {
+        return m_max_size;
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    inline storage_t<key_type, value_type, traits_type>::duration_t storage_t<key_type, value_type, traits_type>::life_time() const noexcept
+    {
+        return m_life_time;
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    void storage_t<key_type, value_type, traits_type>::erase(const key_t & key)
+    {
+        const std::lock_guard guard{ m_lock };
+        auto found = m_index.find(key);
+
+        if (found != m_index.end())
+        {
+            assert(found->second->key == key);
+            m_data.erase(found->second);
+            m_index.erase(found);
+        }
+
+        clear_outdated();
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    inline void storage_t<key_type, value_type, traits_type>::clear() noexcept
+    {
+        try
+        {
+            const std::lock_guard guard{ m_lock };
+            m_index.clear();
+            m_data.clear();
+        }
+        catch (...)
+        {
+            // unexpected exception
+            assert(false);
+        }
+    }
+
+
+    template <class key_type, class value_type, class traits_type>
+    void storage_t<key_type, value_type, traits_type>::clear_outdated()
+    {
+        const time_point_t now = clock_t::now();
+
+        while (!m_data.empty())
+        {
+            const item_t & last = m_data.back();
+            if (last.end_time > now)
+                break;
+
+            m_index.erase(last.key);
+            m_data.pop_back();
+        }
+
+        assert(m_data.size() == m_index.size());
+    }
+
+
+}   // namespace kvstor
---
+++ tests/kvstor_test.cpp
@@ -0,0 +1,277 @@
+﻿#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "kvstor.h"
+#include "doctest.h"
+
+#include <future>
+#include <string>
+
+
+TEST_CASE("kvstor zero size")
+{
+    using stor_t = kvstor::storage_t<long, std::string>;
+    stor_t stor{ 0, std::chrono::hours(24) };
+    REQUIRE(stor.max_size() == 0);
+    REQUIRE(stor.life_time() == std::chrono::hours(24));
+    
+    stor.push(1, "10");
+    stor.push(2, "20");
+    REQUIRE(stor.size() == 0);
+
+    bool no_items = true;
+    auto enum_items = [&no_items](long, std::string, stor_t::duration_t)
+    {
+        no_items = false;
+    };
+
+    stor.map(enum_items);
+    REQUIRE(no_items);
+}
+
+
+TEST_CASE("kvstor::push()")
+{
+    kvstor::storage_t<int, std::string> stor{ 4, std::chrono::hours(24) };
+    REQUIRE(stor.size() == 0);
+
+    stor.push(1, "10");
+    REQUIRE(stor.size() == 1);
+    REQUIRE(stor.find(1).value() == "10");
+
+    stor.push(2, "20");
+    REQUIRE(stor.size() == 2);
+    REQUIRE(stor.find(2).value() == "20");
+
+    stor.push(3, "30");
+    REQUIRE(stor.size() == 3);
+    REQUIRE(stor.find(3).value() == "30");
+
+    stor.push(2, "22");
+    REQUIRE(stor.size() == 3);
+    REQUIRE(stor.find(2).value() == "22");
+
+    stor.push(4, "40");
+    REQUIRE(stor.size() == 4);
+    REQUIRE(stor.find(4).value() == "40");
+
+    stor.push(5, "50");
+    REQUIRE(stor.size() == 4);
+    REQUIRE(stor.find(5).value() == "50");
+    REQUIRE(!stor.find(1).has_value());
+
+    stor.push(6, "60");
+    REQUIRE(stor.size() == 4);
+    REQUIRE(stor.find(6).value() == "60");
+    REQUIRE(!stor.find(3).has_value());
+}
+
+
+TEST_CASE("kvstor::erase()")
+{
+    kvstor::storage_t<size_t, size_t> stor{ 10, std::chrono::hours(24) };
+
+    stor.push(1, 10);
+    stor.push(2, 20);
+    stor.push(3, 30);
+    stor.push(4, 40);
+    REQUIRE(stor.size() == 4);
+    REQUIRE(stor.find(3).value() == 30);
+
+    stor.erase(3);
+    REQUIRE(stor.size() == 3);
+    REQUIRE(!stor.find(3).has_value());
+}
+
+
+TEST_CASE("kvstor::first()")
+{
+    kvstor::storage_t<size_t, std::string> stor{ 10, std::chrono::hours(24) };
+    REQUIRE(!stor.first().has_value());
+
+    stor.push(1, std::string("A"));
+    REQUIRE(stor.first().value() == "A");
+
+    stor.push(2, std::string("B"));
+    REQUIRE(stor.first().value() == "B");
+
+    stor.push(3, std::string("C"));
+    REQUIRE(stor.first().value() == "C");
+
+    stor.erase(100);
+    REQUIRE(stor.first().value() == "C");
+
+    stor.erase(3);
+    REQUIRE(stor.first().value() == "B");
+
+    stor.erase(3);
+    REQUIRE(stor.first().value() == "B");
+
+    stor.erase(1);
+    REQUIRE(stor.first().value() == "B");
+
+    stor.erase(2);
+    REQUIRE(!stor.first().has_value());
+}
+
+
+TEST_CASE("kvstor::last()")
+{
+    kvstor::storage_t<size_t, std::string> stor{ 10, std::chrono::hours(24) };
+    REQUIRE(!stor.last().has_value());
+
+    stor.push(1, std::string("A"));
+    REQUIRE(stor.last().value() == "A");
+
+    stor.push(2, std::string("B"));
+    REQUIRE(stor.last().value() == "A");
+
+    stor.push(3, std::string("C"));
+    REQUIRE(stor.last().value() == "A");
+
+    stor.erase(100);
+    REQUIRE(stor.last().value() == "A");
+
+    stor.erase(1);
+    REQUIRE(stor.last().value() == "B");
+
+    stor.erase(1);
+    REQUIRE(stor.last().value() == "B");
+
+    stor.erase(3);
+    REQUIRE(stor.last().value() == "B");
+
+    stor.erase(2);
+    REQUIRE(!stor.last().has_value());
+}
+
+
+TEST_CASE("kvstor::empty() / clear()")
+{
+    kvstor::storage_t<std::string, std::string> stor{ 10, std::chrono::hours(24) };
+    REQUIRE(stor.empty());
+
+    const std::string s1 = "AAA";
+    const std::string s2 = "BBB";
+    
+    stor.push(s1, s1);
+    REQUIRE(!stor.empty());
+
+    stor.push(s2, s2);
+    REQUIRE(!stor.empty());
+
+    stor.clear();
+    REQUIRE(stor.empty());
+
+    stor.push(s1, s2);
+    REQUIRE(!stor.empty());
+}
+
+
+TEST_CASE("kvstor::map()")
+{
+    using list_t = std::list<std::pair<int, int>>;
+    using stor_t = kvstor::storage_t<int, int>;
+
+    list_t data;
+
+    auto dump_data = [&data](int key, int value, stor_t::duration_t)
+    {
+        data.emplace_back(key, value);
+    };
+
+    const list_t expected_1 = { {4, 40}, {3, 30}, {2, 20}, {1, 10}, };
+
+    kvstor::storage_t<int, int> stor{ 10, std::chrono::hours(24) };
+    stor.push(1, 10);
+    stor.push(2, 20);
+    stor.push(3, 30);
+    stor.push(4, 40);
+
+    stor.map(dump_data);
+    REQUIRE(data == expected_1);
+
+    auto double_it = [](int, int& value, stor_t::duration_t)
+    {
+        value = 2 * value;
+    };
+
+    const list_t expected_2 = { {4, 80}, {3, 60}, {2, 40}, {1, 20}, };
+
+    stor.map(double_it);
+    data.clear();
+    stor.map(dump_data);
+    REQUIRE(data == expected_2);
+}
+
+
+TEST_CASE("kvstor time check")
+{
+    kvstor::storage_t<long, long> stor{ 10, std::chrono::milliseconds(500) };
+
+    stor.push(1, 10);
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    REQUIRE(stor.find(1).value() == 10);
+
+    stor.push(2, 20);
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    REQUIRE(stor.find(1).value() == 10);
+    REQUIRE(stor.find(2).value() == 20);
+    REQUIRE(stor.size() == 2);
+
+    stor.push(3, 30);
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    REQUIRE(!stor.find(1).has_value());
+    REQUIRE(stor.find(2).value() == 20);
+    REQUIRE(stor.find(3).value() == 30);
+    REQUIRE(stor.size() == 2);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    REQUIRE(!stor.find(2).has_value());
+    REQUIRE(stor.find(3).value() == 30);
+    REQUIRE(stor.size() == 1);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    REQUIRE(!stor.find(3).has_value());
+    REQUIRE(stor.empty());
+}
+
+
+TEST_CASE("kvstor thread-safe")
+{
+    constexpr size_t max_count = 20000;
+    constexpr size_t half_count = max_count / 2;
+    constexpr size_t value_factor = 100;
+    
+    kvstor::storage_t<size_t, size_t> stor{ max_count, std::chrono::hours(24) };
+
+    auto fill_storage = [](kvstor::storage_t<size_t, size_t>& stor, size_t first, size_t last, size_t value_factor)
+    {
+        for (size_t key = first; key < last; ++key)
+        {
+            const size_t value = value_factor * key;
+            stor.push(key, value);
+        }
+    };
+
+    auto tst1 = std::async(std::launch::async, fill_storage, std::ref(stor), 0, half_count, value_factor);
+    auto tst2 = std::async(std::launch::async, fill_storage, std::ref(stor), half_count, max_count, value_factor);
+
+    tst1.wait();
+    tst2.wait();
+
+    REQUIRE(stor.size() == max_count);
+
+    for (size_t key = 0; key < max_count; ++key)
+    {
+        const size_t expected = value_factor * key;
+        const auto found = stor.find(key);
+
+        if (!found.has_value())
+            FAIL("key is missed: ", key);
+        else if (found.value() != expected)
+            FAIL("invalid value: ", found.value(), " for key: ", key);
+    }
+
+    const auto not_found = stor.find(max_count);
+    REQUIRE(!stor.find(max_count).has_value());
+}
